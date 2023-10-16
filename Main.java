84a1c9fd61ceab9c805f0a102acf32aa24f22c48
mengawwubetu <mengaww2ya@gmail.com>
2023-10-16 11:15:57 -0700

    that is

---
+++ Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        class student {
+            String name;
+            int age;
+            int id;
+            int midterm;
+            int finalmark;
+        }
+        System.out.println("this is the sample data for one student");
+        student stud = new student(); // Create a new instance of the student class
+        stud.name = "abebe";
+        stud.age = 15;
+        stud.id = 1;
+        stud.midterm = 30;
+        stud.finalmark = 50;
+
+        int total = stud.midterm + stud.finalmark;
+
+        if (total >= 80)
+          {System.out.println("name "+stud.name);
+              System.out.println("age "+stud.age);
+              System.out.println("id "+stud.id);
+           System.out.println("Grade is 'A'");}
+        else if (total >= 70) {
+            System.out.println("name "+stud.name);
+            System.out.println("age "+stud.age);
+            System.out.println("id "+stud.id);
+            System.out.println("Grade is 'B'");
+        }
+        else if (total >= 50)
+        {System.out.println("name "+stud.name);
+            System.out.println("age "+stud.age);
+            System.out.println("id "+stud.id);
+            System.out.println("Grade is 'C'");
+        }
+        else
+        {
+            System.out.println("name "+stud.name);
+            System.out.println("age "+stud.age);
+            System.out.println("id "+stud.id);
+            System.out.println("Grade is 'D'");
+
+        };
+    };
+};
